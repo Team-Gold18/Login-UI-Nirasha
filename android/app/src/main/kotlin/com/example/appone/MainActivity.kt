e9d9aece59e173dd6b6a52e411f87dddc23439ef
Nirasha Wimalasooriya <73355364+Nirash729@users.noreply.github.com>
2022-01-22 13:32:10 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/appone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'loginScreen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Login UI',
+      debugShowCheckedModeBanner: false,
+      //home:LoginScreen(),
+      home:AnimatedSplashScreen(
+        splash:Image.asset(
+          'assets/images/logo.png'
+        ),
+            nextScreen: LoginScreen(),
+        splashTransition: SplashTransition.fadeTransition,
+        ) ,
+
+      );
+
+  }
+}
+
+/*
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => LoginScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[700],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo here
+            Image.asset(
+              'assets/images/logo.png',
+              height: 120,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
+*/
